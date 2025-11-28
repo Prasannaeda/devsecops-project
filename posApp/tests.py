5080eb0e3fa0711203335f7ceb7694971ffb2af2
Prasannaeda <x23304723@student.ncirl.ie>
2025-11-28 12:22:44 +0000

    tests

--- posApp/tests.py
+++ posApp/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class BasicTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_login_page_loads(self):
+        response = self.client.get(reverse("login"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_redirect_admin(self):
+        response = self.client.get(reverse("redirect-admin"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_home_page_redirects(self):
+  
+        response = self.client.get(reverse("home-page"))
+        self.assertIn(response.status_code, [200, 302])
+
+    def test_test_page_loads(self):
+        response = self.client.get(reverse("test-page"))
+        self.assertIn(response.status_code, [200, 302])  # safe for both
