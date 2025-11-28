96d787dbd089c2a05b1b8e26d8726a529d630494
Prasannaeda <x23304723@student.ncirl.ie>
2025-11-28 13:45:31 +0000

    test

--- easy_bill/settings.py
+++ easy_bill/settings.py
@@ -3,16 +3,21 @@ All Django settings here
 """
 import os
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+def get_secret_key():
+    return get_random_secret_key()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--w(dug*va_megpa25w$gmv8o6+f8!fg_*m1^u$)z25pab0hicm'
+SECRET_KEY = get_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
