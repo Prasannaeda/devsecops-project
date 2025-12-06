83001cf43b8259aad6de9857f067ff8f335fc3a0
Prasannaeda <x23304723@student.ncirl.ie>
2025-12-06 13:36:09 +0000

    test

--- easy_bill/settings.py
+++ easy_bill/settings.py
@@ -8,21 +8,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 """
 random secret key
 """
-def get_secret_key():
-    return get_random_secret_key()
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = get_secret_key()
+SECRET_KEY = get_random_secret_key()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['http://myapp-devops.us-east-1.elasticbeanstalk.com/login/?next=/','*']
-
 
 # Application definition
 
